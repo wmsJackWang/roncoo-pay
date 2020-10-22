ab25616df963c2ee16a711adcf3470d06f8abd8c
Wang MingSheng(支付中心) <mingshengwang@sohu-inc.com>
2020-10-22 18:32:48 +0800

    中台项目  网站seo 站长认证

--- roncoo-pay-jackdking-middleplatformservices/src/main/java/org/roncoo/pay/jackdking/middleplatformservices/modules/wechat/controller/WxDomainRightController.java
+++ roncoo-pay-jackdking-middleplatformservices/src/main/java/org/roncoo/pay/jackdking/middleplatformservices/modules/wechat/controller/WxDomainRightController.java
@@ -1,5 +1,8 @@
 package org.roncoo.pay.jackdking.middleplatformservices.modules.wechat.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/wx")
 public class WxDomainRightController {
-
 	
+	private static Map<String,String> sizemap = new HashMap<String, String>();
+	static {
+		sizemap.put("baidu_verify_code-ehN9lzWbui.html", "2ca8182a472f28ec6598b29f501c0e5f");
+		sizemap.put("baidu_verify_code-L413BKVJUv.html", "299be29684e71b56d8a5fe0bc7e8082e");
+		sizemap.put("sogousiteverification.txt", "nkFsnNbJWt");//搜狗平台
+		
+	}
+
+	//百度站长平台验证
+	@ResponseBody
+	@RequestMapping("/baiduVerify/{filename:[_a-zA-Z0-9\\-\\.]+}")//openIdDomain
+	public String baiduVerify(@PathVariable("filename") String filename) {
+
+		//百度站长平台验证
+		return sizemap.get(filename);
+	}
 	
 	//验证获取openid的接口域名权限
 	@ResponseBody
